0c349d9e4507aff1176f772b523fd3ddb748ff5a
arisupriatna14 <arisupriatna703@gmail.com>
2018-07-11 17:35:58 +0700

    done

--- js-racer.js
+++ js-racer.js
@@ -1,7 +1,27 @@
 "use strict"
 
-function diceRoll () {
+const argv = process.argv
+const jumlahPlayer = argv[2]
+const panjangTrack = argv[3]
 
+if (jumlahPlayer >= 2 && panjangTrack >= 15) {
+  const board = printBoard()
+  console.log(board)
+} else {
+  console.log('Jumlah player atau panjang track tidak valid!');
+}
+
+function diceRoll () {
+  return Math.floor(Math.random() * 5) + 1
+}
+
+function generatePlayers () {
+  const players = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase()
+  let playerObj = {}
+  for (let i = 0; i < jumlahPlayer; i++) {
+    playerObj[players[i]] = 0
+  }
+  return playerObj
 }
 
 function sleep (milliseconds) {
@@ -14,27 +34,54 @@ function sleep (milliseconds) {
 }
 
 function printBoard () {
+  const player = generatePlayers()
+  let winner = ''
 
+  while (winner === '') {
+    let board = []
+    for (let key in player) {
+      if (player[key] === 0) {
+        let trackLine = printLine(key, player[key])
+        player[key] += diceRoll()
+        board.push(trackLine)
+      } else {
+        let lemparDaduPlayer = diceRoll(player[key])
+        if (winner === '') {
+          if (player[key] + lemparDaduPlayer >= panjangTrack-1) {
+            player[key] = panjangTrack -1
+            winner = key;
+          } else {
+            player[key] += lemparDaduPlayer
+          }
+        }
+        let mainBoard = printLine(key, player[key])
+        board.push(mainBoard)
+      }
+    }
+    console.log(board);
+    sleep();
+    clearScreen();
+  }
+  return winners(winner)
 }
 
 function printLine (player, pos) {
+  let board = []
 
+  for (let i = 0; i < panjangTrack; i++) {
+    if (i === pos) {
+      board.push(player)
+    } else {
+      board.push(' ')
+    }
+  }
+  return board.join('|')
 }
 
-function advance (player) {
-
-}
-
-function finished () {
-
-}
-
-function winner () {
-
+function winners (winner) {
+  return `============== The Winner is ${winner} ==============`
 }
 
 function clearScreen () {
-  // Un-comment this line if you have trouble with console.clear();
-  // return process.stdout.write('\033c');
   console.clear();
 }
